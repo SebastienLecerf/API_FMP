89337472815bd96482b8dfcb022f1b98c3867450
Sébastien Lecerf <seb.lecerf@gmail.com>
2014-12-02 18:33:01 +0100

    dfdfdsfs

--- fmi-test/API Examples/listScripts.php
+++ fmi-test/API Examples/listScripts.php
@@ -34,7 +34,7 @@ require_once("../FileMaker.php");
 // If FMSA web server is on another machine, specify 'hostspec' as follows:
 //   $fm = new FileMaker('FMPHP_Sample', 'http://10.0.0.1');
 $fm = new FileMaker('FMPHP_Sample','http://localhost');
-$fm->setProperty('username', 'Sadmi');
+$fm->setProperty('username', 'Admin');
 $fm->setProperty('password', 'symfony76');
 
 // Call listScripts() to get array of script names.
